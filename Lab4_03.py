703be569350be0c9bc0981513318cfa52ddd6fa5
AITI Student <aiti-student@AITI-83.(none)>
2012-06-22 16:01:04 +0000

    data structures lab

--- Lab4_03.py
+++ Lab4_03.py
@@ -1,12 +1,15 @@
 # a list data structure can be used
-in_stock = ['bananas', 'strawberies','apples','bread']
+in_stock = ['Bananas', 'Strawberies','Apples','Bread']
 in_stock.sort()
 
 always_in_stock = tuple(in_stock)
-#always_in_stock[2]='yam'
+#always_in_stock[2]='yam'               testing if list is now tuple 
 #in_stock[2]='yam'
 #for smtin in in_stock:
 #    print smtin
+print 'Come to Shoprite! We always sell'
+print
+
 for item in always_in_stock:
     print item,'Always in Stock'
 
