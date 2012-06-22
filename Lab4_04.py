41d7a368cd65d48306cff7c4569f37d281120cb3
AITI Student <aiti-student@AITI-83.(none)>
2012-06-22 17:08:07 +0000

    data structures lab

---
+++ Lab4_04.py
@@ -0,0 +1,23 @@
+# data structure used will be dictionary in dictionary
+"""Store = {'shop1':{'Apple':1.5,'Banana':0.25,'Strawberries':5,'Carrots':0.5,'Bread':1.0},'shop2':{'Apple':2,'Banana':0.15,'Strawberries':2,'Carrots':0.5,'Bread':2.0}}
+
+print Store['shop1']
+"""
+
+#using list in dictionary data structure
+
+store = {'Apple':[1.5,2], 'Banana':[0.25,0.15], 'Carrots':[0.5,0.5], 'Strawberries':[5,2]}
+print store['Apple']
+
+def binary_insert(newfloat,some_list_of_floats):
+    return
+
+binary_insert(3,store['Apple'])
+print 'an apple is',store['Apple']
+
+
+"""
+#tryin another fxn
+
+di=dict(zip("Apple Banana Strawberries Carrots " )split(),[1.5,2])
+""
