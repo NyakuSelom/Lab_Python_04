850cf85b0f54c4985d83199f8d14b1d15590d5a8
AITI Student <aiti-student@AITI-83.(none)>
2012-06-22 15:53:13 +0000

    data structures lab

--- Lab4_03.py
+++ Lab4_03.py
@@ -1,1 +1,14 @@
-# the best data structure to use for this question is dictionary
+# a list data structure can be used
+in_stock = ['bananas', 'strawberies','apples','bread']
+in_stock.sort()
+
+always_in_stock = tuple(in_stock)
+#always_in_stock[2]='yam'
+#in_stock[2]='yam'
+#for smtin in in_stock:
+#    print smtin
+for item in always_in_stock:
+    print item,'Always in Stock'
+
+# if the items do not change then Tupple data structures can be used
+# convert list to tupple datastructure
